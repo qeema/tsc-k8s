94a84cbbd0b483f38bcb3533d5f6032883b0c46d
qeema <qeemakaree@gmail.com>
2020-11-30 17:51:05 +0900

    Add request

--- functions/lib/index.js
+++ functions/lib/index.js
@@ -1,11 +1,29 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.helloWorld = void 0;
+exports.request = exports.response = void 0;
 const functions = require("firebase-functions");
-exports.helloWorld = functions.https.onRequest((request, response) => {
+const axios_1 = require("axios");
+exports.response = functions.https.onRequest((request, response) => {
     console.log('===================================');
     console.log(request.headers['x-appengine-user-ip']);
+    console.log(request.headers);
     console.log('-----------------------------------');
     response.send('Hello from Firebase!\n\n');
+});
+exports.request = functions
+    .runWith({
+    memory: '512MB',
+    timeoutSeconds: 100,
+})
+    .region('asia-northeast1')
+    .pubsub.schedule('* * * * *')
+    .onRun(() => {
+    void (async () => {
+        const res = await axios_1.default({
+            method: 'get',
+            url: 'http://localhost:5001/test-acb17/us-central1/response',
+        });
+        console.log(res.data);
+    })();
 });
 //# sourceMappingURL=index.js.map
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,29 @@
 import * as functions from 'firebase-functions';
+import axios from 'axios';
 
-export const helloWorld = functions.https.onRequest((request, response) => {
+export const response = functions.https.onRequest((request, response) => {
   console.log('===================================');
   console.log(request.headers['x-appengine-user-ip']);
+  console.log(request.headers);
   console.log('-----------------------------------');
   response.send('Hello from Firebase!\n\n');
 });
+
+export const request = functions
+  .runWith({
+    memory: '512MB',
+    timeoutSeconds: 100,
+  })
+  .region('asia-northeast1')
+  .pubsub.schedule('* * * * *')
+  .onRun(() => {
+    void (async () => {
+      const res = await axios({
+        method: 'get',
+        //url: 'http://localhost:5001/test-acb17/us-central1/response',
+        url: 'https://us-central1-test-acb17.cloudfunctions.net/response',
+      });
+
+      console.log(res.data);
+    })();
+  });
